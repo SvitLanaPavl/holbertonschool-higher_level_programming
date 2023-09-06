ae1462693f7e2196d168f81743785e8c328d65da
Svitlana Pavlovska <97898647+SvitLanaPavl@users.noreply.github.com>
2023-09-06 11:28:36 -0500

    output string added

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
+    output = ""
     for ch in str:
         if ch in 'abcdefghijklmnopqrstuvwxyz':
             a = ord(ch)
             b = a - 32
-    print("{}".format(chr(b)))
-    
+            output = output+chr(b)
+        else:
+            output = output+ch
+    print("{}".format(output), end="")
