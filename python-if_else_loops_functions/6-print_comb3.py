5fae5ff60ebb54f0ce1bcb1b7304f0359ce70bfe
Svitlana Pavlovska <97898647+SvitLanaPavl@users.noreply.github.com>
2023-09-06 10:50:41 -0500

    ending tweaking

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i != 8 and j != 9:
-            print("{}{}, ".format(i, j), end="")
-print(89)
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2), end="")
+    print("{}{}".format(num1, num2), end=", ")
