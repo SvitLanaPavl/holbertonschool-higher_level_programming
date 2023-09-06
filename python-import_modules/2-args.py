d6bd2fa5f361a6465e6ec682d514c9e0e3ad4cea
Svitlana Pavlovska <97898647+SvitLanaPavl@users.noreply.github.com>
2023-09-06 18:26:51 -0500

    formatting

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 0:
-    print("No arguments")
+if (len(sys.argv) - 1) == 0:
+    print("0 arguments.")
 else:
     if (len(sys.argv) - 1) == 1:
-        print("{} argument".format(len(sys.argv) - 1))
+        print("{} argument:".format(len(sys.argv) - 1))
     else:
-        print("{} arguments".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
