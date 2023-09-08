e5da974ac91c38a20dc6809941aecd13128b2a49
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-08 13:13:30 -0700

    checking if the list exists

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list[::-1]
-    for element in new_list:
-        print("{:d}".format(element))
+    if my_list:
+        new_list = my_list[::-1]
+        for element in new_list:
+            print("{:d}".format(element))
