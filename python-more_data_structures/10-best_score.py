f6fa57f17719064bd6a5967911d1f3e3cc5e77f6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-10 13:39:03 -0700

    First method added on return

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     max_value = max(a_dictionary.values())
-    return [key for key, value in a_dictionary.items() if value == max_value]
+    return [key for key, value in a_dictionary.items() if value == max_value][0]
