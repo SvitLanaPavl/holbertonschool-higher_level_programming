7802d435fbc82a54c242c2db0d71cc0731a8fbb6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-10 16:13:26 -0700

    Getting around len in return statement of except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
+        for index, value in enumerate(my_list):
+            if index < x:
+                print(value, end="")
+                i += 1
+            else:
+                break
         print()
-        return x
+        return i
     except IndexError:
         print()
-        return len(my_list)
+        return i
