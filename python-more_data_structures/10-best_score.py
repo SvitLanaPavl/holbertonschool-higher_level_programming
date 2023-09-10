a261510e82d4764182aca684f87dc6f3a1101ac6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-10 13:43:14 -0700

    Filling up the requirement to return in case no best key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     max_value = max(a_dictionary.values())
-    return [key for key, value in a_dictionary.items() if value == max_value][0]
+    best_key = [key for key, value in a_dictionary.items() if value == max_value]
+    if not best_key:
+        return None
+    else:
+        return best_key[0]
