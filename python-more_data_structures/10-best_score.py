8e7d6a0d718dd107dea63dda4df4027df8969566
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-10 14:35:52 -0700

    Changed condition on empty dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None:
         return None
     max_value = max(a_dictionary.values(), default=None)
     best_key = [key for key, value in a_dictionary.items() if value == max_value]
