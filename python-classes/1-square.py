15ba59c7d28a713076b3b3ccd83d3cb9ddb075c6
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-14 13:16:50 -0700

    First try

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines a square."""
+
+
+class Square:
+    """It represents a square."""
+    __size = None
+
+    def __init__(self, size):
+        """Initialize the square"""
+        self.__size = size
