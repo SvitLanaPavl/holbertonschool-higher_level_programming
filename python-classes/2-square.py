b7ece2fe0587da22f23bcaf6fab8cd1f6d3a9a9b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-14 13:40:51 -0700

    some exceptions raise

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a square."""
+
+
+class Square:
+    """It represents a square."""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initializes size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
