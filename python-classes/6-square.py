65971384e2c27a15efc6416a4ce287616bf29554
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-14 15:12:05 -0700

    Spaces

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -53,7 +53,7 @@ class Square:
                 print()
             for i in range(self.__size):
                 for x in range(self.__position[0]):
-                    print("_", end="")
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
