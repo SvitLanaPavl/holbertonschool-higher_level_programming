507ab16cf8bac18b5e89850def0f1a9c9cef587f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-15 12:21:39 -0700

    first commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Function that adds two integers and returns their
+    sum as an integer
+
+    Raises:
+    TypeError: if 'a' or 'b' is not a number
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(round(a + b))
