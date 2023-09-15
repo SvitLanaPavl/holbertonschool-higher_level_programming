623ca4dca1b8d8dbf10fa129dd48c92f6a41a314
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-15 09:19:14 -0700

    Trying

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,14 +52,11 @@ class Square:
         """Prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
-        elif self.__position is None:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            for y in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+        for x in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for x in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
