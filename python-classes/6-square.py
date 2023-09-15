df8cc8087cdbe38c43e53d20e0911f89a341de4e
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-15 09:20:35 -0700

    Trying

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,6 +52,7 @@ class Square:
         """Prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
+            return
         for x in range(self.__position[1]):
             print()
         for i in range(self.__size):
