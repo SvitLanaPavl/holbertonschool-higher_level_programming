465179c27fc71b6ada74fcb319228c1fd0eabe0a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-21 12:31:32 -0700

    First try

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""MyList inherits from list"""
+
+
+class MyList(list):
+    """the class inherits from list"""
+    
+    def print_sorted(self):
+        """Sorts the list"""
+        self.sort()
