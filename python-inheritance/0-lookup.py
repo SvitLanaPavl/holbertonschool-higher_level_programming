0b1fdbb474d919c19d21eb300a07f034a66411eb
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-21 12:11:40 -0700

    Much simpler way??

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def lookup(obj):
     """Returns a list of available attributes"""
-    attrs = []
-    for attr in dir(obj):
-        attrs.append(attr)
-    return attrs
+    return dir(obj)
