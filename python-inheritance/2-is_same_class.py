4633a2e17278d6226d0fd0d8190f3cc0364679f4
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-22 09:39:24 -0700

    Precise type, not just isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Object is instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """True if the object exactly an instance of the specified class"""
+    return type(obj) is a_class
