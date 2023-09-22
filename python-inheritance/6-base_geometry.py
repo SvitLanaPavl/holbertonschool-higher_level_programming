e07613f2dbb6954a8ed77f3b245faf2386149400
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-22 12:16:59 -0700

    raising exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+
+    def area(self):
+        """public instance method that raises exception"""
+        raise Exception("area() is not implemented")
