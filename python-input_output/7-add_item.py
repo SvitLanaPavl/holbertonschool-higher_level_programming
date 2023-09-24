87b73008cdc5c9ee47a5cf4fea27a50990cf4942
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 21:39:14 -0700

    spacing

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Adds items to a file"""
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__(' 6-load_from_json_file').load_from_json_file
 
+open("add_item.json", "w", encoding="utf-8")
 try:
     list = load_from_json_file("add_item.json")
-except FileNotFoundError:
+except ValueError:
     list = []
 
 # adding args
