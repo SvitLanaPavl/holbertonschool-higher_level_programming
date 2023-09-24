bc38bee89405ee5cb702864333216ebf8de3d290
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 19:31:40 -0700

    magic methods

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from Base Geometry"""
+
+    def __init__(self, width, height):
+        """
+        Initialization
+
+        Args:
+        width: the width of the rectangle (private)
+        height: the height of the rectangle (private)
+        """
+        self.__width = width
+        self.__height = height
+
+        # Validate the width and height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Returns area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns string representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __print__(self):
+        """prints the string"""
+        print(self)
