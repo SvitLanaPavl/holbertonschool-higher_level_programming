033d830fcc2dad4b761c270cbbf49e80a8b4159b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 21:36:30 -0700

    some modifications with except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,17 +4,14 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__(' 6-load_from_json_file').load_from_json_file
 
+try:
+    list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    list = []
 
-def add_items_to_file(filename, items):
-    """Adds items to a file"""
-    my_list = load_from_json_file(filename)
-    if my_list is None:
-        my_list = []
-    for item in items:
-        my_list.append(item)
-    save_to_json_file(my_list, filename)
+# adding args
+for arg in sys.argv[1:]:
+    list.append(arg)
 
-if __name__ == "__main__":
-    items = sys.argv[1:]
-
-add_items_to_file("add_item.json", items)
+# save to json
+save_to_json_file(list, "add_item.json")
