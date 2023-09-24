5ece1a53c364e51dd84235be8c44fff2ba109e78
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 21:47:13 -0700

    opening specification

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,15 +6,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__(' 6-load_from_json_file').load_from_json_file
 
-open("add_item.json", "a")
-try:
-    list = load_from_json_file("add_item.json")
-except ValueError:
-    list = []
 
-# adding args
-for arg in sys.argv[1:]:
-    list.append(arg)
-
-# save to json
-save_to_json_file(list, "add_item.json")
+if __name__ == '__main__':
+    try:
+        list = load_from_json_file("add_item.json")
+    except:
+        list = []
+    for arg in sys.argv[1:]:
+        list.append(arg)
+    save_to_json_file(list, "add_item.json")
