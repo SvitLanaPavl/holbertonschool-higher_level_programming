d3833448d3abe8334d2fc6ec8b718f76ebfd3949
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 19:41:33 -0700

    REmoved print method?

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -29,7 +29,3 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Returns string representation of the rectangle"""
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
-
-    def __print__(self):
-        """prints the string"""
-        print(self)
