79a389ca1da7add97affd003c6f35e643dffaf8b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 22:07:42 -0700

    way simpler

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,19 +4,4 @@
 
 def class_to_json(obj):
     """Returns dictionary description"""
-    json_dict = {}
-    for attr in obj.__dict__:
-        value = getattr(obj, attr)
-        if isinstance(value, list):
-            json_dict[attr] = [class_to_json(item) for item in value]
-        elif isinstance(value, dict):
-            json_dict[attr] = [class_to_json(value)]
-        elif isinstance(value, str):
-            json_dict[attr] = value
-        elif isinstance(value, int):
-            json_dict[attr] = value
-        elif isinstance(value, bool):
-            json_dict[attr] = value
-        else:
-            raise TypeError("Attribute {} is not serializable.".format(attr))
-    return json_dict
+    return (obj.__dict__)
