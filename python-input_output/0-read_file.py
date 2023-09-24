25d07442388c6e09a2f0dc42011af0885d6c0b8c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 20:20:44 -0700

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """Reads the text file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line.rstrip())
