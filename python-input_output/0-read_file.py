352d99337b2d14ad6d92252c7a8b8725fa8ae038
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-23 20:30:27 -0700

    doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 def read_file(filename=""):
     """Reads the text file"""
 
-    with open(filename, "r", encoding='UTF8') as f:
+    with open(filename, "r", encoding='utf-8') as f:
         for line in f:
             print(line, end="")
