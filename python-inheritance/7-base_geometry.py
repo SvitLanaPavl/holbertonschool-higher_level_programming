d6e08b70c680d1ff57382cea1e0d10c23d7e08c7
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-25 08:05:51 -0700

    something removed

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,12 +13,12 @@ class BaseGeometry:
         """public instance method that validates value
 
         Args:
-        name: name
-        value: value
+            name: name
+            value: value
 
         Raises:
-        TypeError: name must ne an integer
-        ValueError: name must be greater that 0
+            TypeError: name must ne an integer
+            ValueError: name must be greater that 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
