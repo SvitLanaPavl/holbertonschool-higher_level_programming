d8f51b9bb9709fee4f5ca9287c6a2250e3c04ac4
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-09-30 19:57:32 -0700

    some tests done, see checks

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -9,19 +9,33 @@ from models.square import Square
 class TestBase(unittest.TestCase):
     '''The subclass of the TestCase to test Base'''
 
-    def test_init_id(self):
+    def test_id(self):
         '''Testing the __init__ method id'''
         b1 = Base()
         self.assertEqual(b1.id, 1)
-        b2 = Base(None)
-        self.assertEqual(b2.id, 2)
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
-        b6 = Base(-1)
-        self.assertEqual(b6.id, -1)
-        b7 = Base("Holberton")
-        self.assertEqual(b7.id, "Holberton")
+    
+    def test_several_id(self):
+        '''Testing several id's''' 
+        b1 = Base(None)
+        self.assertEqual(b1.id, 2)
+        b2 = Base()
+        self.assertEqual(b2.id, 3)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 4)
+
+    def test_negative_id(self):
+        '''negative id'''
+        b1 = Base(-1)
+        self.assertEqual(b1.id, -1)
+
+    def test_string_id(self):
+        '''string id''' 
+        b1 = Base("Holberton")
+        self.assertEqual(b1.id, "Holberton")
+
+    def test_arg(self):
+        '''many args'''
+        with self.assertRaises(TypeError):
+            b1 = Base(1, 1)
--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,12 +7,41 @@ from models.square import Square
 
 class TestRectangle(unittest.TestCase):
     '''The subclass of the TestCase to test Rectangle'''
-    def test_init_id(self):
+    def test_init_param(self):
         '''Testing the __init__ method'''
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
         r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
         self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
 
+    def test_value_type(self):
+        '''data type value errors'''
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, "2")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2)
+            r.width = -10
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.x = {}
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2, 3, -1)
+
+    def test_arg(self):
+        '''few args'''
+        with self.assertRaises(TypeError):
+            r = Rectangle(1)
