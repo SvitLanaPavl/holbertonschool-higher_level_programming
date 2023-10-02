8dee2f90c29b2968bf660f75cb0c01cc70bb4377
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-02 15:46:53 -0700

    without the string in return

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base:
             list_dictionaries: a list of dictionaries
         """
         if list_dictionaries is None or list_dictionaries == "[]":
-            return "[]"
+            return []
         return json.dumps(list_dictionaries)
 
     @classmethod
