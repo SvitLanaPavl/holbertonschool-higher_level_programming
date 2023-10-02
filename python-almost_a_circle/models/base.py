70eb8dc5bf6b272fd6ad299f7cda6028d1029d0f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-02 15:52:59 -0700

    condition changed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
         Args:
             list_dictionaries: a list of dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
