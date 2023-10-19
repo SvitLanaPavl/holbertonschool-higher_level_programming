95899e76a39e6c3c17dfdb90cd64b470ea7750e1
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-19 13:39:15 -0700

    task 8 - not quotes but back quotes

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Displays the number of records
-SELECT COUNT ('id') FROM first_table WHERE id = 89
+-- Displays the number of records with id = 89
+SELECT COUNT (`id`) FROM first_table WHERE id = 89
