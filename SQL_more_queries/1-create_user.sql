af813994012f5fce658a350160ca3c06462a7a0e
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 09:27:01 -0700

    with grant option back

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,5 +4,5 @@ IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 ON *.* TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+WITH GRANT OPTION;
 FLUSH PRIVILEGES;
