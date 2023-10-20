b6f656277ad551552f2510581463e12070a8e67f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 10:39:06 -0700

    remove extra on dot notation

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities of California that can be found on the database
 SELECT hbtn_0d_usa.cities.id, hbtn_0d_usa.cities.name
-WHERE state_id IN (SELECT states.id WHERE name = California) ORDER BY cities.id ASC;
+WHERE state_id IN (SELECT states.id WHERE name = California) ORDER BY id ASC;
