df83c1df567a6d6979c288ef7ce69b55489884f0
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 09:56:30 -0700

    shorter description

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creates the table force_name on MySQL server
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256)) WHERE name IS NOT NULL;
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
