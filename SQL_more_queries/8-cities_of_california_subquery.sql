bba2b84ce80611e6f3b518a67bbcfa27ebb2ce55
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 10:45:06 -0700

    instead of dot notation I use FROM

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- Lists all the cities of California that can be found on the database
-SELECT cities.id, cities.name
-WHERE state_id = (SELECT states.id WHERE name = 'California');
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California');
