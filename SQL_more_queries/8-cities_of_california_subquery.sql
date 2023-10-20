8194a3e0771c079ac7817071a41397036cfd2b3f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 10:41:53 -0700

    no asc

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities of California that can be found on the database
 SELECT cities.id, cities.name
-WHERE state_id IN (SELECT states.id WHERE name = 'California') ORDER BY id ASC;
+WHERE state_id IN (SELECT states.id WHERE name = 'California') ORDER BY id;
