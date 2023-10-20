e228be464f8f1cefd618ee32b1d1834e6bf676ea
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 10:11:42 -0700

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates database hbtn_0d_usa, the table states on MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT AUTO GENERATED PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
