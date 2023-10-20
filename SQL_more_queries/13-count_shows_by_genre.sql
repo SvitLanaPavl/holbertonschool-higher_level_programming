41f0b4618b93182716db6abdbbda6876b1812ecc
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-20 14:13:48 -0700

    group by genre which is specified after as

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_show_genres.name AS genre,
 COUNT(show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_shows_genres ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
