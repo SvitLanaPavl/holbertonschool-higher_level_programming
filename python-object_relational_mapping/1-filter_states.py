73255d0f48acd1f0963cbd638f9f7ea44c4ca0cf
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 11:42:53 -0700

    N is in the condition inside loop before print

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%")
+    cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
