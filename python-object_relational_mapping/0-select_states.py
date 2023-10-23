ec7cb494ed1692b4aa01110f28fd35159b8f264f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 10:55:17 -0700

    typo

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     '''Lists all states from the database hbtn_0e_0_usa'''
 
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
