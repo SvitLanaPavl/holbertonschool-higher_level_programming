f432dec14e752e5873399e4d5b67fbdd1502d945
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 12:17:50 -0700

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name
+matches the argument safe from injections'''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    '''Lists all states from the database hbtn_0e_0_usa'''
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{:s}'".format(argv[4])
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == argv[4]:
+            print(state)
