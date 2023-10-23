1288aea5eaaf075b671e2ec38aeeba5c3054f521
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:33:50 -0700

    import differently

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,7 @@
 '''lists all State objects from the database hbtn_0e_6_usa'''
 from sys import argv
 from model_state import State, Base
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
