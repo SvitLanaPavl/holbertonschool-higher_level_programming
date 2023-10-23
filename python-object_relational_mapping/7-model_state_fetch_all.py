86410573741cef5dc3b2e8a61a5f5763c781602c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:43:47 -0700

    forgot to rename the file back

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,4 +14,3 @@ if __name__ == '__main__':
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print(f'{state.id}: {state.name}')
-    session.close()
