131b6752893c503e366cb9f29c333a87ba70dd3c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:21:57 -0700

    reodered imports

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''lists all State objects from the database hbtn_0e_6_usa'''
+from sys import argv
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-from sys import argv
 
 
 if __name__ == '__main__':
