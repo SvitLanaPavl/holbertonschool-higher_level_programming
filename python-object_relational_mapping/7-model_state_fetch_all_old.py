d588847cf895b71e5fe9a06c61d198421c486d6a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:42:52 -0700

    OMG it was typo mYsqldb

--- python-object_relational_mapping/7-model_state_fetch_all_old.py
+++ python-object_relational_mapping/7-model_state_fetch_all_old.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine(f'mysql+msqldb://{argv[1]}:{argv[2]}\
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
                            @localhost:3306/{argv[3]}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
