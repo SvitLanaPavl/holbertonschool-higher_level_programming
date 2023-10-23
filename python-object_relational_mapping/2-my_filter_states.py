5e84be21d4d8ea5f2685905797c1fa6d03eed3be
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 11:58:46 -0700

    pycodestyling

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,12 +9,13 @@ from sys import argv
 if __name__ == '__main__':
     '''Lists all states from the database hbtn_0e_0_usa'''
 
-    username, password, database, state_name = argv[1], argv[2], argv[3], argv[4]
-    db = MySQLdb.connect(host='localhost', port=3306, user=username,
-                         passwd=password, db=database)
+    ur, pw, db, s_n = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=ur,
+                         passwd=pw, db=db)
     cursor = db.cursor()
-    query = 'SELECT * FROM states WHERE name = %s ORDER BY id'
-    cursor.execute(query, (state_name,))
+    query = 'SELECT * FROM states WHERE name = {%s} ORDER BY id'.format(s_n)
+    cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
-        print(state)
+        if state[1] == s_n:
+            print(state)
