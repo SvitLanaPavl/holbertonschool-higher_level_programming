0c65b988b90a0a486296ab2a042d72ab8c8c486d
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 13:00:19 -0700

    query change

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    query = '''SELECT cities.id, cities.name, states.name
+    query = '''SELECT cities.name
             FROM cities
             JOIN states ON cities.state_id = states.id
             ORDER BY cities.id ASC;
