eb9a486bcb4dbd844b5666ef6ed1441bfc60a39f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 14:52:18 -0700

    removed Base

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,7 @@
 '''lists all State objects from the database hbtn_0e_6_usa'''
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 from sys import argv
 
 
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     engine = create_engine(f'mysql+msqldb://{argv[1]}:{argv[2]}\
                            @localhost:3306/{argv[3]}',
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).all()
