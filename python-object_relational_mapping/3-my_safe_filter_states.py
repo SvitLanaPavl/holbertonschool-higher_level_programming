ebefc5461d79843f60b508ef184059dd1dc06b73
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 12:24:30 -0700

    just removed specifier from the query - it is still there in the loop condition

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{:s}'".format(argv[4])
+    query = "SELECT * FROM states"
     cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
