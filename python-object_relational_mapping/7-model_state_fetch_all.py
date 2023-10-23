198177d5778731c1b844603f4b4329d6644123c8
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 14:47:36 -0700

    pool_pre_ping to test connection for liveness

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,9 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    engine = create_engine(f'mysql+pymsql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    engine = create_engine(f'mysql+pymsql://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}',
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
