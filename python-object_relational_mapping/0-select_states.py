7c55ae5979b71ddb491650509d7d83bfb9ced36f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 10:35:18 -0700

    first with argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 '''The module documentation'''
 import MySQLdb
+from sys import argv
 
 
-def list_states(username, password, database):
-    '''Lists all states from the database hbtn_0e_0_usa
-    
-    Args:
-    username: MySQL username
-    password: MySQL password
-    database: the databse name
+if __name__ == '__main__':
+    '''Lists all states from the database hbtn_0e_0_usa'''
 
-    Return: list of tuple, in each: id and name
-    '''
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute('SELECT id, name FROM states ORDER BY id ASC')
+    cursor.execute('SELECT * FROM states')
     states = cursor.fetchall()
-    cursor.close()
-    db.close()
-    return states
+    for state in states:
+        print(state)
