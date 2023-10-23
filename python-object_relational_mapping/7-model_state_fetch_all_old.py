abae100cb0cab3160b3803a0b270958e3c72ea92
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:37:12 -0700

    trying this way

--- python-object_relational_mapping/7-model_state_fetch_all_old.py
+++ python-object_relational_mapping/7-model_state_fetch_all_old.py
@@ -6,14 +6,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+msqldb://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+        print(f'{state.id}: {state.name}')
     session.close()
