bd621989c0028a20a464157202010834fdd5291f
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 12:03:05 -0700

    back to simplified version(removed variables), no condition insode print, no order by id

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,13 +9,11 @@ from sys import argv
 if __name__ == '__main__':
     '''Lists all states from the database hbtn_0e_0_usa'''
 
-    ur, pw, db, s_n = argv[1], argv[2], argv[3], argv[4]
-    db = MySQLdb.connect(host='localhost', port=3306, user=ur,
-                         passwd=pw, db=db)
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    query = 'SELECT * FROM states WHERE name = {%s} ORDER BY id'.format(s_n)
+    query = 'SELECT * FROM states WHERE name = {%s}'.format(argv[4])
     cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
-        if state[1] == s_n:
-            print(state)
+        print(state)
