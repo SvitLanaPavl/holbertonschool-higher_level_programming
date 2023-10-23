bf055a8acf3a9edba5b796768161c804980f79a0
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 14:43:03 -0700

    there should have been created a session object

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''lists all State objects from the database hbtn_0e_6_usa'''
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
@@ -9,7 +9,8 @@ from sys import argv
 if __name__ == '__main__':
     engine = create_engine(f'mysql+pymsql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
     Base.metadata.create_all(engine)
-    session = engine.connect()
+    Session = sessionmaker(engine)
+    session = Session()
     states = session.query(State).order_by(State.id).all()
     for state in states:
         print(f'{state.id}: {state.name}')
