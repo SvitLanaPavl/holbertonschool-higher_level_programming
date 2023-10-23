5dec691c48213122ea40535da662694f31493339
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 14:33:21 -0700

    task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''lists all State objects from the database hbtn_0e_6_usa'''
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+pymsql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    Base.metadata.create_all(engine)
+    session = engine.connect()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f'{state.id}: {state.name}')
+    session.close()
