4508678fd1cc342746db92cffbe63709f8070c0b
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 10:52:05 -0700

    no localhost or port

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == 'main':
     '''Lists all states from the database hbtn_0e_0_usa'''
 
-    db = MySQLdb.connect(user=argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states')
