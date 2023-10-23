a28f2596466d9d12ad12ae321f8209dbd4a86571
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:52:54 -0700

    correct presentation

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     session = engine.connect()
     state = session.query(State).order_by(State.id).first()
     if state is not None:
-        print(state.name)
+        print(f'{state.id}: {state.name}')
     else:
         print('Nothing')
