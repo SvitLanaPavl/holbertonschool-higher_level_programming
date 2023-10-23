b99fc43bc1450a3146db4f7cb4a9355c6ad945d4
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 14:18:58 -0700

    there were supposed to be parantheses on declarative_base

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,9 +5,11 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declarative_base
+Base = declarative_base()
+
+
 class State(Base):
-    '''Inhertis from Base'''
+    '''Class that inhertis from Base'''
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
