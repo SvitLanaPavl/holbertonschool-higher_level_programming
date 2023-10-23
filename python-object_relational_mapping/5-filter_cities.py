6576d6abf2b5e534f9fe3bd1574b0552d748b54a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 12:47:32 -0700

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''takes in an argument and displays all cities
+in the states table of hbtn_0e_0_usa where name
+matches the argument safe from injections'''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    '''Lists all states from the database hbtn_0e_0_usa'''
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    query = '''SELECT cities.id, cities.name, states.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC;
+            '''
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for item in states:
+        if item[1] == argv[4]:
+            print(item)
