e8be68d9cf49e71942f46ae17540c43cff9736a2
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 16:56:15 -0700

    copied something from the previous task on session maker

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,13 +3,15 @@
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
     engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
                            @localhost:3306/{argv[3]}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     state = session.query(State).order_by(State.id).first()
     if state is not None:
         print(f'{state.id}: {state.name}')
