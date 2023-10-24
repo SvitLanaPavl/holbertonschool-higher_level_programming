a2ba90d6fec1bd0b0b86bdae37277543edb1dced
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 17:05:07 -0700

    pycodestyling

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''lists all State objects that contain the letter a from the database hbtn_0e_6_usa'''
+'''lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa'''
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.contains('a')).order_by(State.id)
+    states = session.query(State).filter(State.name.contains('a'))\
+        .order_by(State.id)
     for state in states:
         print(f'{state.id}: {state.name}')
