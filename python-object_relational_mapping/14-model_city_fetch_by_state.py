9fa12f5cf8c4bacf33a4dca029e6dd0fe4088e79
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-10-23 17:54:31 -0700

    task 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''lists all State objects from the database hbtn_0e_6_usa'''
+from sys import argv
+from model_state import State, Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City).join(State).order_by(City.id).all()
+    for city in cities:
+        print(f'{city.state.name}: ({city.id}) {city.name}')
