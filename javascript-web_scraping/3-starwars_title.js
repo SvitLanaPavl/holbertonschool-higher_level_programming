837850ef4849a95a9b12d8cd6041a477110a394d
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-01 16:47:39 +0000

    task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+request(url, (error, jsonData) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(jsonData).title);
+  }
+});
