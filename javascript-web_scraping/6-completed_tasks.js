786e7b503265ee372bb47faf8c903514fb916028
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 03:54:29 +0000

    task 6 first try

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, _, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const infoSet = {};
+    tasks.forEach(task => {
+      if (task.completed) {
+        const userId = task.userID;
+        infoSet[userId] = (infoSet[userId] || 0);
+        infoSet[userId]++;
+      }
+    });
+    for (const userId in infoSet) {
+      if (infoSet[userId] > 0) {
+        console.log(`"${userId}": ${infoSet[userId]}`);
+      }
+    }
+  }
+});
