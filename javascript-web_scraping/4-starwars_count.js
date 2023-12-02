bca3ffc6c328d233d1f2613164cc16b8b77adc8a
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 03:14:11 +0000

    substituting url with process.argv since not working on local server

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 request(url, (error, _, body) => {
   if (error) {
     console.error(error);
