00bfe2cc8e0c06456e62de2f845cdedc5f7cd52a
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 02:46:01 +0000

    again url error - specified properly

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/people/';
+const url = 'https://swapi-api.hbtn.io/api/films/';
 const id = 18;
 request(url, (error, _, body) => {
   if (error) {
@@ -8,7 +8,7 @@ request(url, (error, _, body) => {
   } else {
     const data = JSON.parse(body).results;
     const films = data.filter((film) =>
-      film.characters.includes(`https://swapi-api.hbtn.io/api/films/people/${id}/`));
+      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${id}/`));
     console.log(films.length);
   }
 });
