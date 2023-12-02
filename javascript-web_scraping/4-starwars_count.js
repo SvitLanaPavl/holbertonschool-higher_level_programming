789ba95a68eb518511df40e4f29765ecf1a7a473
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 02:42:30 +0000

    people added to initial url

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.hbtn.io/api/films/people/';
 const id = 18;
 request(url, (error, _, body) => {
   if (error) {
