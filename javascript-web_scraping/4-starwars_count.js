0eb9bdf4379e50b2e9d316b80840f66494148264
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 03:12:33 +0000

    trying to tackle with a count and a loop

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-const id = 18;
 request(url, (error, _, body) => {
   if (error) {
     console.error(error);
   } else {
     const data = JSON.parse(body).results;
-    const films = data.filter((film) =>
-      film.characters.includes(`https://swapi-api.hbtn.io/api/people/${id}/`));
-    console.log(films.length);
+    let count = 0;
+    for (const set of data) {
+      for (const character of set.characters) {
+        if (character.includes('18')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
   }
 });
