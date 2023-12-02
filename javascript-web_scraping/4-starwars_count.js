f0f77fc3abf735033553beccf5c550859daf265e
Pavlovska Svitlana <lanapavlovska90@gmail.com>
2023-12-02 02:38:55 +0000

    chmod forgotten

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,13 +6,9 @@ request(url, (error, _, body) => {
   if (error) {
     console.error(error);
   } else {
-    try {
-      const data = JSON.parse(body).results;
-      const films = data.filter((film) =>
-        film.characters.includes(`https://swapi-api.hbtn.io/api/films/people/${id}/`));
-      console.log(films.length);
-    } catch (parseError) {
-      console.error(parseError);
-    }
+    const data = JSON.parse(body).results;
+    const films = data.filter((film) =>
+      film.characters.includes(`https://swapi-api.hbtn.io/api/films/people/${id}/`));
+    console.log(films.length);
   }
 });
